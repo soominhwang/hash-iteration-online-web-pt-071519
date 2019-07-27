7815f3a9b8d177c192eb295e982c886fab97080f
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 23:41:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,7 +2,6 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12
       "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else ""
     end
   end
 end
