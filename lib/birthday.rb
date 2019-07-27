9692cc02d4738f893097b3fad1b23f735e16ed67
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 23:43:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,6 @@ def happy_birthday(birthday_kids)
     if age < 12
       congrats << "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
-    congrats.join
+    return congrats.join
   end
 end
